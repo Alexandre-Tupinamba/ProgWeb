10122bd4b968dab4517b5a85006a76adb5b3d9c4
Alexandre-Tupinamba <60014649+Alexandre-Tupinamba@users.noreply.github.com>
2022-11-28 20:26:46 -0400

    Update script.js
    Gera conteúdo html automaticamente

--- javaScript1/atividade1/script.js
+++ javaScript1/atividade1/script.js
@@ -1,1 +1,9 @@
+document.write("<h1>Instituto de Computação</h1>");
 
+for (var i = 1; i < 11; i++) {
+    document.write("<table border=1 ><thead><th colspan = 2>produtos de "+i+" </th></thead><tbody>");
+     for (var j = 1; j < 11; j++) {
+    document.write("<tr><td align=center>"+i+"X"+j+"</td><td align=center>"+i*j+"</td></tr>");
+     }
+     document.write("</tbody></table>");
+    }
